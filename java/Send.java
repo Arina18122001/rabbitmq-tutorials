3ea71c6ae5a9f7005316be4c2b74f66351bc4b0f
Arina18122001 <smirnova.arinushka@yandex.ru>
2025-02-20 14:35:40 +0300

    Update Send.java

--- java/Send.java
+++ java/Send.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Send {
 
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "notification";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -14,7 +14,7 @@ public class Send {
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
+            String message = "Заказ передан в доставку";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         }
